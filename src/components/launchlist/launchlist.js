3ece773ab11495389ecf53d5750a57e7a22957d7
chandu11b4 <chandu4it@gmail.com>
2020-08-19 20:31:00 +0530

    2020-08-19 changes

--- src/components/launchlist/launchlist.js
+++ src/components/launchlist/launchlist.js
@@ -3,7 +3,8 @@ import {Launch} from '../launch/launch.js';
 import axios from 'axios';
 class LaunchList extends Component{
 state={
-launchs:{}
+launchs:{},
+limit:105
 }
 componentDidMount = () =>{
 axios.get('https://api.spacexdata.com/v3/launches')
@@ -22,10 +23,15 @@ axios.get('https://api.spacexdata.com/v3/launches')
   });
 }
 
+loadMore = () =>{
+	this.setState({limit:this.state.limit+5});
+}
 displayLaunchs = () =>{
 
-const z = Object.keys(this.state.launchs).map( x =>{
-		  		return (<Launch title="FalconSat" img="https://farm9.staticflickr.com/8617/16789019815_f99a165dc5_o.jpg" date="2020-02-05" desc="Engine failure at 33 seconds and loss of vehicle"/>)
+const z = Object.keys(this.state.launchs).slice(0,this.state.limit).map( (x,i) =>{
+const img = this.state.launchs[x].links.flickr_images.length===0 ? "https://farm9.staticflickr.com/8617/16789019815_f99a165dc5_o.jpg" : this.state.launchs[x].links.flickr_images[0]; 
+		  		return (<Launch key={"launch"+i} title={this.state.launchs[x].mission_name} img={img} date={this.state.launchs[x].launch_date_local} desc={this.state.launchs[x].details} />)
+		  		
 		  	}) 
 		  	return z;
 }
@@ -35,6 +41,7 @@ render(){
 
 	return(<div>
 		{this.displayLaunchs()}
+		<button onClick={this.loadMore}>More</button>
 	</div>)
 }
 }
